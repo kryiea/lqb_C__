dc7ba7085d8796ef5b7556b2263c8a576b229e9d
kryiea <1582877056@qq.com>
2023-01-23 15:44:22 +0800

    1.23

---
+++ cmake-build-debug/CMakeFiles/P1177.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/86195/Desktop/Lqb_C++/P1177.cpp" "CMakeFiles/P1177.dir/P1177.cpp.obj" "gcc" "CMakeFiles/P1177.dir/P1177.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/P1177.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/P1177.dir/P1177.cpp.obj"
+  "CMakeFiles/P1177.dir/P1177.cpp.obj.d"
+  "P1177.exe"
+  "P1177.exe.manifest"
+  "P1177.pdb"
+  "libP1177.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/P1177.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
